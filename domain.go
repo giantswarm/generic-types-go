87b1bdd79eb8a4e2470fbce36b49cd768297e526
Tim Schindler <tim@giantswarm.io>
2015-05-19 09:31:13 +0000

    removed user-config specific code

--- domain.go
+++ domain.go
@@ -10,22 +10,6 @@ import (
 
 type Domain string
 
-type DomainConfig map[Domain]DockerPort
-
-// ToSimple just maps the domain config with its custom types to a more simpler
-// map. This can be used for internal management once the validity of domains
-// and ports is given. That way dependencies between packages requiring hard
-// custom types can be dropped.
-func (dc DomainConfig) ToSimple() map[string]string {
-	simpleDomains := map[string]string{}
-
-	for d, p := range dc {
-		simpleDomains[d.String()] = p.Port
-	}
-
-	return simpleDomains
-}
-
 func (d *Domain) MarshalJSON() ([]byte, error) {
 	return json.Marshal(d.String())
 }
